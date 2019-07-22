46c6bbd239d7fdd6378aab21960cebfe0f406f1e
Alice Brunel <alc.brunel@gmail.com>
2019-07-22 09:53:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,7 @@ class Song
   end 
   
   def self.find_or_create_by_name(name)
-    if find_by_name(name) == true
+    if find_by_name(name) != nil
       self.find_by_name(name)
     else
       self.create_by_name(name)
