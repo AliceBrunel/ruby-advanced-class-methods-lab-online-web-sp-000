b2e27d1d40b1fe3a6140d01856f71823c93b64b0
Alice Brunel <alc.brunel@gmail.com>
2019-07-22 08:22:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,13 +16,13 @@ class Song
   end
 
   def self.new_by_name(name)
-    song = self.new
+    song = self
     song.name = name
   end 
   
 
   def self.create_by_name(name)
-    song = self
+    song = self.new
     song.name = name
     @@all << song
   end 
